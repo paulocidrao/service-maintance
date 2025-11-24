5b90d118d666ae0f5ef7a4e928f28be5e77b80e5
Paulo Cidrão <paulocdrao.123@gmail.com>
2025-11-23 23:41:28 -0300

    feat:add modal

--- app/components/CardDemand/CardComponent.vue
+++ app/components/CardDemand/CardComponent.vue
@@ -1,4 +1,6 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ModalComponent from "../Modal/ModalComponent.vue";
+</script>
 
 <template>
   <UCard class="w-1/4">
@@ -8,7 +10,7 @@
     <template #default>
       <section class="flex flex-col gap-1">
         <p>Computador com x defeitos</p>
-        <span>Codigo do cliente: 000-00</span>
+        <span>Código do cliente: 000-00</span>
         <span>Data de entrega: dd/mm/yyyy</span>
         <span>Valor do serviço: R$ 00,00</span>
         <span class="flex items-center gap-1"
@@ -20,7 +22,14 @@
     </template>
     <template #footer>
       <section class="flex justify-between">
-        <UButton class="cursor-pointer" color="neutral">Atualizar</UButton>
+        <ModalComponent
+          money="00,00"
+          description="Computador com x defeitos"
+          date="dd/mm/yyyy"
+          status="Aprove"
+          code="000-00"
+          title="Cliente XPTO"
+        />
         <UButton class="cursor-pointer">Finalizar serviço</UButton>
       </section>
     </template>
---
+++ app/components/Modal/ModalComponent.vue
@@ -0,0 +1,90 @@
+<script lang="ts" setup>
+const statusValue = ref(["Done", "Pending", "Cancel", "Aprove"]);
+defineProps<{
+  title: string;
+  code: string;
+  description: string;
+  money: string;
+  date: string;
+  status: "Done" | "Pending" | "Cancel" | "Aprove";
+}>();
+</script>
+
+<template>
+  <UModal
+    :dismissible="false"
+    :title="title"
+    :ui="{
+      content: 'bg-card',
+      overlay: 'bg-black/60 transition-opacity',
+      close: 'hover:bg-black',
+    }"
+  >
+    <UButton label="Atualizar" color="neutral" class="cursor-pointer" />
+    <template #body="{ close }">
+      <section>
+        <UForm class="flex flex-col space-y-4">
+          <UFormField
+            size="xl"
+            label="Descrição do problema"
+            name="description"
+          >
+            <UInput
+              color="neutral"
+              aria-label="Atualize a descrição do serviço"
+              placeholder="Digite o novo nome do serviço"
+              highlight
+              class="w-full"
+              :default-value="description"
+              autofocus
+            />
+          </UFormField>
+          <UFormField size="xl" name="money" label="Orçamento">
+            <UInput
+              aria-label="Atualize o valor do seu orçamento"
+              placeholder="Digite um novo valor de orçamento"
+              color="neutral"
+              highlight
+              :default-value="money"
+              class="w-full"
+            />
+          </UFormField>
+          <UFormField size="xl" name="code" label="Código do cliente">
+            <span class="">{{ code }}</span>
+          </UFormField>
+          <UFormField size="xl" label="Data de entrega" name="date">
+            <UInput
+              color="neutral"
+              aria-label="Atualize a sua data de entrega"
+              placeholder="Digite uma nova data de entrega"
+              highlight
+              class="w-full"
+              type="date"
+              :default-value="date"
+            />
+          </UFormField>
+          <UFormField size="xl" label="Status" name="status">
+            <USelect
+              color="neutral"
+              aria-label="Selecione um novo status para o serviço"
+              highlight
+              class="w-full"
+              type="item"
+              :default-value="status"
+              :items="statusValue"
+              :ui="{
+                group: 'bg-card',
+              }"
+            />
+          </UFormField>
+          <UButton
+            class="items-center justify-center flex cursor-pointer font-bold"
+            color="neutral"
+            @click="close"
+            >Atualizar</UButton
+          >
+        </UForm>
+      </section>
+    </template>
+  </UModal>
+</template>
