a1c2ee67753028daf879ac70c75afbb0267a5ac7
Paulo Cidrão <paulocdrao.123@gmail.com>
2025-11-24 21:47:41 -0300

    feat: add track page

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import HeaderComponent from "~/components/Header/HeaderComponent.vue";
+const routes = useRoute();
+console.log(routes.path);
 </script>
 
 <template>
   <UApp>
-    <HeaderComponent />
+    <HeaderComponent v-show="routes.path !== '/track'" />
     <NuxtPage />
   </UApp>
 </template>
--- app/pages/Track.vue
+++ app/pages/Track.vue
@@ -1,8 +1,23 @@
 <script lang="ts" setup></script>
 <template>
-  <UContainer class="flex justify-center items-center">
-    <section class="flex w-1/4 items-center justify-center mt-16 bg-card">
-      <UPinInput color="neutral" highlight placeholder="o" :length="5" />
+  <UContainer class="flex h-screen justify-center items-center">
+    <section
+      class="flex flex-col space-y-4 rounded p-4 bg-card items-center w-full max-w-sm mx-auto min-h-[250px] justify-around"
+    >
+      <h1 class="text-2xl text-white font-bold">Busque pelo seu serviço</h1>
+      <UPinInput
+        size="xl"
+        class="gap-5"
+        color="neutral"
+        highlight
+        :length="5"
+      />
+
+      <UButton
+        label="Verificar serviço"
+        color="neutral"
+        class="justify-center font-bold flex w-full"
+      />
     </section>
   </UContainer>
 </template>
