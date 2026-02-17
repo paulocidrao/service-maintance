5e73c0f7f7d1d37f34ad37cb2ad72e55364325f0
Paulo Cidrão <paulocdrao.123@gmail.com>
2026-02-17 13:15:13 -0300

    feat add 404 page

---
+++ app/error.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useError } from '#app'
+const errorPage = useError();
+</script>
+
+<template>
+  <NuxtLayout name="404">
+    <div>
+      <div class="flex items-center justify-center text-white gap-1.5">
+        <h2 class="text-2xl ">Erro: {{ errorPage?.statusCode }}</h2>
+        <UIcon name="i-lucide-bot" class="size-12 " />
+      </div>
+      <p class="text-xl text-white font-semibold">A página {{ errorPage?.url }}</p>
+    </div>
+    <NuxtLink to="/" class="bg-white p-2 rounded text-black font-bold mt-4">Ir para página inicial</NuxtLink>
+  </NuxtLayout>
+</template>
---
+++ app/layouts/404.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="w-full flex items-center justify-center flex-col h-screen">
+    <h1 class="text-3xl font-bold text-white ">Oops! página não encontrada!</h1>
+    <slot />
+  </section>
+</template>
+
+<style scoped></style>
